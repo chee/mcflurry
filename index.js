84cd1895a401b378aa226dd27d0cb7d333ceb33b
chee <chee@snoot.club>
2019-08-14 20:54:45 +0100

    yeet

--- index.js
+++
@@ -1,9 +0,0 @@
-module.exports = mcflurry
-mcflurry.default = mcflurry
-
-function mcflurry(fn, object = {}) {
-  return function(next) {
-    const current = Object.assign({}, object, next)
-    return next ? mcflurry(fn, current) : fn(current)
-  }
-}
---
+++ readme.md
@@ -0,0 +1,1 @@
+Any further development on mcflurry will happen on https://git.snoot.club/chee/mcflurry
